803c89f45f1f8517174813c62a77fd41caa14616
William S <25758160+WillGITCode@users.noreply.github.com>
2018-03-29 21:23:56 -0400

    fix ads

--- src/GoogleAd.js
+++ src/GoogleAd.js
@@ -10,12 +10,13 @@ export default class GoogleAd extends Component{
         return (
           <div className='ad'>
             <ins className='adsbygoogle'
-              data-adtest="on"
               style={{ display: 'block' }}
               data-ad-client='ca-pub-2259939651078522'
-              data-ad-slot='8903129752'
+              data-ad-slot='2384690961'
               data-ad-format='auto' />
           </div>
         )
       }
 }
+
+
